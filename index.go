fb992adf555863aeda5184119a9e55923cedefba
Chetan Gowda <chetan.hs@gmail.com>
2013-03-27 23:44:00 -0700

    [OTFC] fix comment in index.go

--- index.go
+++ index.go
@@ -1,6 +1,6 @@
 package otfc
 
-// Represents an index record. The offset
+// Represents an index record. The offset points to the actual config data in the data block
 type indexRecord struct {
 	key     [16]byte //md5 hash of the config key
 	offset  uint32
